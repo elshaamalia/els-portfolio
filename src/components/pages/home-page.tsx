81b86711a60a0863a59bff33c0c4c0de0578a42d
elsha amalia <elshaaml14@gmail.com>
2025-12-03 23:59:18 +0700

    fix

--- src/components/pages/home-page.tsx
+++ src/components/pages/home-page.tsx
@@ -1,10 +1,15 @@
 "use client"
-import { Quicksand } from "next/font/google"
+import { Quicksand, Montserrat } from "next/font/google"
 import Particles from "../animations/Particles"
 
 const quicksand = Quicksand({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
+})
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 })
 
 export default function HomePage() {
@@ -28,19 +33,19 @@ export default function HomePage() {
       <div className="max-w-6xl space-y-6 relative z-10 text-center flex flex-col items-center justify-center">
         <div className="space-y-0 w-full flex flex-col items-center">
           
-          <p className={`text-slate-300 text-2xl tracking-[0.3em] w-full text-left pl-3 ${quicksand.className}`}>
-            HELLO! WELCOME TO
+          <p className={`text-slate-300 text-5xl tracking-[0.5em] w-full text-left pl-6 ${montserrat.className}`}>
+            エルシャ
           </p>
           {/* mix-blend-screen: INI KUNCINYA. */}
           {/* Ini bikin warna Hitam (bg-black) jadi TRANSPARAN 100% terhadap background particles */}
-          <div className="relative mt-[-1rem] bg-black mix-blend-screen select-none">
+          <div className="relative -mt-6 bg-black mix-blend-screen p-4 select-none outline outline-4 outline-black">
             
             {/* 1. Teks (Warna Putih) */}
             {/* Teks putih ini akan jadi 'wadah' buat videonya */}
             <h1
-              className="text-[220px] font-black tracking-tight leading-none text-white bg-black relative z-0"
+              className={`text-[250px] font-black tracking-wide leading-none text-white bg-black relative z-0 ${montserrat.className}`}
             >
-              ELSHA&apos;S
+              ELSHA
             </h1>
 
             {/* 2. Video (mix-blend-multiply) */}
@@ -58,9 +63,9 @@ export default function HomePage() {
             </video>
             
           </div>
-          <p className={`mt-2 text-2xl text-slate-300 tracking-[0.3em] w-full text-right ${quicksand.className}`}>
+          {/* <p className={`mt-2 text-2xl text-slate-300 tracking-[0.3em] w-full text-right ${quicksand.className}`}>
             PORTFOLIO
-          </p>
+          </p> */}
         </div>
       </div>
     </div>
